472188b79f3674240f645e084f778b80beeafb48
leehwui <leehwui@163.com>
2026-02-15 14:50:10 +0800

    update frontend title

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Tangerine Photo",
+  title: "Leehwui Photo",
   description: "Photography Portfolio",
   icons: {
     icon: "/icon.png",
